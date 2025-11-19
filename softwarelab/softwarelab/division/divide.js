af02a54e2a813798fc9234809ecff7f9ff6a0b41
Sulav Adhikari <141142171+sulav063@users.noreply.github.com>
2025-11-19 07:19:54 +0545

    Delete softwarelab/softwarelab directory

--- softwarelab/softwarelab/division/divide.js
+++
@@ -1,6 +0,0 @@
-function divideNumbers(a, b) {
-  if (b === 0) return { success: false, message: "Division by zero error" };
-  return { success: true, result: a / b };
-}
-
-module.exports = { divideNumbers };
